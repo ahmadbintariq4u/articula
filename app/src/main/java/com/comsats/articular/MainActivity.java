1fc3191b260955966e104f56ccd5fd3cc0971b83
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2021-04-21 13:07:03 +0500

    Making Splash Screen

--- app/src/main/java/com/comsats/articular/MainActivity.java
+++ app/src/main/java/com/comsats/articular/MainActivity.java
@@ -2,7 +2,14 @@ package com.comsats.articular;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.hanks.htextview.base.HTextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        ImageView image=findViewById(R.id.splash_person);
+        image.setImageResource(R.drawable.ic_splash_person);
+
+        HTextView text=findViewById(R.id.splash_text);
+        text.animateText("A Place Where You Write Your Article.");
+
+
+        HTextView text2=findViewById(R.id.splash_title);
+        text2.animateText("Articular");
+        text2.setLinkTextColor(Color.parseColor("#ffffff"));
+
+
+       // Toasty.error(this, "This is an error toast.", Toast.LENGTH_SHORT, true).show();
+
     }
-}
+} 
