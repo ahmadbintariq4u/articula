62d5368b5bbe5396e75e8b9acf3b49343286f508
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2021-04-24 16:39:58 +0500

    Adding FAB and Option menus

--- app/src/main/java/com/comsats/articular/HomeActivity.java
+++ app/src/main/java/com/comsats/articular/HomeActivity.java
@@ -1,14 +1,51 @@
 package com.comsats.articular;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.comsats.articular.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // set the listener to the FAB.
+        binding.homeFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this, AddArticle.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        Toast.makeText(this, "Item selected: "+item.getTitle(), Toast.LENGTH_SHORT).show();
+        return true;
+
+    }
+
 }
--- app/src/main/java/com/comsats/articular/MainActivity.java
+++ app/src/main/java/com/comsats/articular/MainActivity.java
@@ -1,14 +1,18 @@
 package com.comsats.articular;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.dynamicanimation.animation.DynamicAnimation;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
 
 
@@ -34,7 +38,17 @@ public class MainActivity extends AppCompatActivity {
         // set the animation to the slogan.
         HTextView text=findViewById(R.id.splash_text);
         text.animateText("A Place Where You Write Your Article.");
+        text.setAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationEnd(HTextView hTextView) {
+                //Toast.makeText(MainActivity.this, "Animation end", Toast.LENGTH_SHORT).show();
 
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
 
 
        // Toasty.error(this, "This is an error toast.", Toast.LENGTH_SHORT, true).show();
